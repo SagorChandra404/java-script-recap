a940af5de7b0b87ece68b6172faae496dd97a625
sagor chandra sutradhar <sagorchandrasutradhar@gmail.com>
2024-11-15 18:56:22 +0600

    added odd even number

--- for.js
+++ for.js
@@ -1,6 +1,10 @@
-for (num = 1; num <= 10; num++) {
+// for (num = 1; num <= 10; num++) {
+//   console.log(num);
+//   if (num % 2 === 0) {
+//     console.log("even number", num);
+//   }
+// }
+
+for (let num = 1; num <= 10; num++) {
   console.log(num);
-  if (num % 2 === 0) {
-    console.log("even number", num);
-  }
 }
